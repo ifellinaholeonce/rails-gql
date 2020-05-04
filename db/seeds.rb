f6ff62240b2c05654c7fc7a31720d76a5221a3cc
Tymm Schmitke <ifellinaholeonce@gmail.com>
2020-05-03 22:37:41 -0400

    item query types

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+
+  Item.create!(
+    [
+      {
+        title: Faker::Books::Lovecraft.tome,
+        description: Faker::Books::Lovecraft.fhtagn,
+        user: user,
+        image_url: "https://www.placecage.com/200/300"
+      }
+    ]
+  )
+end
